c8e026d5e42cebf636ece66f810a1e91c58112aa
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-06 08:45:51 -0700

    Add course and type controlled fields to form

--- scripts/roundForm.js
+++ scripts/roundForm.js
@@ -7,12 +7,14 @@ class RoundForm extends React.Component {
     constructor(props) {
       super(props);
       const today = new Date(Date.now()-(new Date()).getTimezoneOffset()*60000);
-      this.state = {date:  today.toISOString().substr(0,10)}; 
+      this.state = {date:  today.toISOString().substr(0,10),
+                    course: ''}; 
     }
   
     handleChange = (event) => {
-        this.setState({date: event.target.value});
-    }
+        const name = event.target.name;
+        this.setState({[name]: event.target.value});
+      }
   
     handleSubmit = (event) => {
       alert('The form was submitted!');
@@ -31,6 +33,17 @@ class RoundForm extends React.Component {
                 </label>
                 <div id="roundDateDescr" className="form-text">
                 Enter a valid date
+                </div>
+            </div>
+            <div className="mb-3 centered">
+                <label htmlFor="roundCourse" className="form-label">Course:
+                    <input id="roundCourse"  className="form-control centered" type="text" 
+                        aria-describedby="roundCourseDescr"
+                        size="50" maxLength="50"  value={this.state.course} 
+                        onChange={this.handleChange} required />
+                </label>
+                <div id="roundCourseDescr" className="form-text">
+                Enter a course name of at most 50 characters
                 </div>
             </div>
             <div className="centered">
