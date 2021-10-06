14f1adaf0d622e3cfe74daffb6c26a204703ef2c
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-05 21:01:31 -0700

    Create initial version of roundForm component empty form with submit button

--- scripts/roundForm.js
+++ scripts/roundForm.js
@@ -16,15 +16,6 @@ class RoundForm extends React.Component {
         return (
         <form id="logRoundForm" 
               onSubmit={this.handleSubmit} noValidate>
-          <div className="mb-3 centered">
-            <label htmlFor="roundDate" className="form-label">Date:
-            <input id="roundDate" className="form-control centered" type="date" 
-                aria-describedby="roundDateDescr" required/>
-            </label>
-            <div id="roundDateDescr" className="form-text">
-            Enter a valid date
-            </div>
-          </div>
           <div className="centered">
             <button type="submit" className="btn btn-primary">
                 <span className="fa fa-save"></span>
@@ -34,8 +25,8 @@ class RoundForm extends React.Component {
         </form>
       );
     }
-}
-
+  }
+  
   ReactDOM.render(
     <RoundForm />,
     document.getElementById('root')
