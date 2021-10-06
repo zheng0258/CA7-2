1653a28b228c7b671cdbe7f71fb087a74140db3d
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-05 21:11:40 -0700

    Update Round form with date field and initialize to current date

--- scripts/roundForm.js
+++ scripts/roundForm.js
@@ -1,6 +1,13 @@
+/*************************************************************************
+ * File: roundForm.js
+ * This file defines a React controlled component for a logging a round.
+ ************************************************************************/
+
 class RoundForm extends React.Component {
     constructor(props) {
       super(props);
+      const today = new Date(Date.now()-(new Date()).getTimezoneOffset()*60000);
+      this.state = {date:  today.toISOString().substr(0,10)}; 
     }
   
     handleChange = (event) => {
@@ -16,11 +23,20 @@ class RoundForm extends React.Component {
         return (
         <form id="logRoundForm" 
               onSubmit={this.handleSubmit} noValidate>
-          <div className="centered">
-            <button type="submit" className="btn btn-primary">
-                <span className="fa fa-save"></span>
-                &nbsp;Log Round
-            </button>
+            <div className="mb-3 centered">
+                <label htmlFor="roundDate" className="form-label">Date:
+                    <input id="roundDate" className="form-control centered" type="date" 
+                       aria-describedby="roundDateDescr" value={this.state.date} required/>
+                </label>
+                <div id="roundDateDescr" className="form-text">
+                Enter a valid date
+                </div>
+            </div>
+            <div className="centered">
+                <button type="submit" className="btn btn-primary">
+                    <span className="fa fa-save"></span>
+                    &nbsp;Log Round
+                </button>
           </div>
         </form>
       );
